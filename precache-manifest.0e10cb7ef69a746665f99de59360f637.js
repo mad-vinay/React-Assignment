e6f868a164cf7b85cad0caf180d85c403e27583f
Vinay Edadan <vinay@qburst.com>
2019-01-10 19:56:39 +0530

    Updates

---
+++ precache-manifest.0e10cb7ef69a746665f99de59360f637.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b509cb4f645738d95a2a",
+    "url": "/shopping-cart/static/js/runtime~main.b509cb4f.js"
+  },
+  {
+    "revision": "e640cff826e6fbe94ef8",
+    "url": "/shopping-cart/static/js/main.e640cff8.chunk.js"
+  },
+  {
+    "revision": "d801bc31b6cc2cae63e8",
+    "url": "/shopping-cart/static/js/1.d801bc31.chunk.js"
+  },
+  {
+    "revision": "e640cff826e6fbe94ef8",
+    "url": "/shopping-cart/static/css/main.6e5237ec.chunk.css"
+  },
+  {
+    "revision": "fc61c76ff69c81ad460f9efd7a9ee9c4",
+    "url": "/shopping-cart/index.html"
+  }
+];
