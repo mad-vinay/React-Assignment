4f0381a730ab402779bf0c45821ba9dcd2fa3212
Vinay Edadan <vinay@qburst.com>
2019-01-28 11:42:01 +0530

    redux

--- src/container/CartItems/index.js
+++ src/container/CartItems/index.js
@@ -2,37 +2,51 @@ import React, { Component } from 'react';
 import './index.css';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { viewCart } from '../../actions/AddAction'
+import { viewCart, counterIncrement, countUpdation } from '../../actions/AddAction'
 
 class CartItems extends Component {
+    
+    deleteItem = (item) => {
+        debugger
+        let {cartItems, itemsInCart} = this.props.data;
+        this.props.counterIncrement(this.props.data.count - 1)
+        cartItems.splice(cartItems.indexOf(item.id, 1))
+        itemsInCart.splice(itemsInCart.indexOf(item.id, 1)) 
+        this.props.countUpdation(itemsInCart)
+        this.props.viewCart(cartItems)
+        // console.log(item)
+    }
 
-  render() {
-    const {items} = this.props.location.state; 
-    console.log(items, '&&&&&&&&&&&&&&&&&&&&')   
-    return (
-        items.map(item =>
-            <li className='listStyle cartItems' key={item.id}>
-                <div className="imageWrap">
-                    <img alt="product img" className="imageStyle" src={item.url}></img>                
-                </div>
-                <div className="prodDetails">
-                    <p className="productName">{item.name}</p>
-                    <p className="priceTag">{item.price}</p>
-                    <span className={ item.shipping===" " ? "paid-shipping" : "shippingText"}>{item.shipping}</span>
-                </div>
-                <div>
-                    <button className="buttonStyle">Place the Order</button>
-                </div>
-            </li>
-          )
-    );
-  }
+    render() {
+        const {items} = this.props.location.state;
+
+        return (
+            items.map(item =>
+                <li className='listStyle cartItems' key={item.id}>
+                    <div className="imageWrap">
+                        <img alt="product img" className="imageStyle" src={item.url}></img>                
+                    </div>
+                    <div className="prodDetails">
+                        <p className="productName">{item.name}</p>
+                        <p className="priceTag">{item.price}</p>
+                        <span className={ item.shipping===" " ? "paid-shipping" : "shippingText"}>{item.shipping}</span>
+                    </div>
+                    <div>
+                        <button className="buttonStyle">Place the Order</button>
+                        <button onClick={this.deleteItem.bind(this, item)} className="buttonStyle deleteItem">Remove from Cart</button>
+                    </div>
+                </li>
+            )
+        );
+    }
 }
 
 function matchDispatchToProps(dispatch) {
   // console.log('matchDispatchToProps', dispatch)
   return bindActionCreators({
-    viewCart
+    viewCart,
+    counterIncrement,
+    countUpdation
   }, dispatch);
 };
 
