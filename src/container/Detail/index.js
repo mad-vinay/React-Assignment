61936a62f2efa570bf48e512cf8750be36834744
Vinay <vinay@qburst.com>
2019-01-31 12:11:30 +0530

    bug fixes

--- src/container/Detail/index.js
+++ src/container/Detail/index.js
@@ -12,7 +12,17 @@ class Detail extends Component {
                     <div className="detailBox">
                         <p>{item.name}</p>
                         <img alt="product" src={item.url}></img>
-                        <h4>Price: {item.price}</h4>
+                    </div>
+                    <div className="description">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                            It has survived not only five centuries, but also the leap into electronic typesetting,
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                            Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                        <h4>Price: <span>${item.price}</span></h4>
                     </div>
                 </div>
             </div>
